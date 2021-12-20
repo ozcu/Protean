1f48cb9b2917aee7daa42347242f8066e7c16484
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-12-20 23:28:40 +0300

    Chest gives coin to Player

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Chest : Collectable
 {
     public Sprite emptyChest;
-    public int coinAmount=10;
+    public int coinAmount= 10;
    protected override void OnCollect(){
        if(!collected){
            collected= true;
            GetComponent<SpriteRenderer>().sprite = emptyChest;
+           GameManager.instance.coins += coinAmount;
            GameManager.instance.ShowText("+" + coinAmount + " coins!",25,Color.yellow,transform.position,Vector3.up * 25,1.5f);
        }
    }
