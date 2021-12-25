a4dc2bda4916e1dd7d349733db9a310eaee55f1b
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-12-25 17:50:18 +0300

    change animation sprite name

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,10 +6,10 @@ public abstract class Mover : Fighter
     protected BoxCollider2D boxCollider;
     protected RaycastHit2D hit;
     protected Vector3 moveDelta;
-    //public Sprite Player_left;
-    public Sprite Player_right;
-    public Sprite Player_up;
-    public Sprite Player_down;
+
+    public Sprite Actor_right;
+    public Sprite Actor_up;
+    public Sprite Actor_down;
 
     protected float xSpeed = 1.0f;
     protected float ySpeed = 0.75f;
@@ -22,39 +22,43 @@ public abstract class Mover : Fighter
  
     protected virtual void moveActor(Vector3 input){
         //Reset moveDelta
-    moveDelta = new Vector3(input.x * xSpeed,input.y *ySpeed,0);
-    
+        moveDelta = new Vector3(input.x * xSpeed,input.y *ySpeed,0);
+        
 
-    //Swap Sprite direction
-    if(moveDelta.x > 0){
-        //this.gameObject.GetComponent<SpriteRenderer>().sprite = Player_right;
-        transform.localScale = new Vector3(1,1,0);
-    }else if(moveDelta.x <0){
-        //this.gameObject.GetComponent<SpriteRenderer>().sprite = Player_left;
-        transform.localScale = new Vector3(-1,1,0);
-    }else if(moveDelta.y >0){
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = Player_up;
-    }else if(moveDelta.y <0){
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = Player_down;
+        //Swap Sprite direction
+        if(moveDelta.x > 0){
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Actor_right;
+            transform.localScale = new Vector3(1,1,0);
+        }else if(moveDelta.x <0){
+            //this.gameObject.GetComponent<SpriteRenderer>().sprite = Actor_left;
+            transform.localScale = new Vector3(-1,1,0);
+        }else if(moveDelta.y >0){
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Actor_up;
+        }else if(moveDelta.y <0){
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Actor_down;
+        }
+
+        //Add Push Vector from Combat
+        moveDelta +=pushDirection;
+        //Reduce the push force according to Push Recovery Speed
+        pushDirection = Vector3.Lerp(pushDirection,Vector3.zero,pushRecoverySpeed);
+        
+
+        //Control for Collision
+        hit = Physics2D.BoxCast(transform.position,boxCollider.size,0,new Vector2(0,moveDelta.y),Mathf.Abs(moveDelta.y * Time.deltaTime),LayerMask.GetMask("Actor","Blocker"));
+        if(hit.collider == null){
+            //Move Player on Y
+            transform.Translate(0,moveDelta.y * Time.deltaTime,0);
+        }
+        hit = Physics2D.BoxCast(transform.position,boxCollider.size,0,new Vector2(moveDelta.x,0),Mathf.Abs(moveDelta.x * Time.deltaTime),LayerMask.GetMask("Actor","Blocker"));
+        if(hit.collider == null){
+            //Move Player on X
+            transform.Translate(moveDelta.x * Time.deltaTime,0,0);
+        }
     }
 
-    //Add Push Vector from Combat
-    moveDelta +=pushDirection;
-    //Reduce the push force according to Push Recovery Speed
-    pushDirection = Vector3.Lerp(pushDirection,Vector3.zero,pushRecoverySpeed);
-    
+   
 
-    //Control for Collision
-    hit = Physics2D.BoxCast(transform.position,boxCollider.size,0,new Vector2(0,moveDelta.y),Mathf.Abs(moveDelta.y * Time.deltaTime),LayerMask.GetMask("Actor","Blocker"));
-    if(hit.collider == null){
-        //Move Player on Y
-        transform.Translate(0,moveDelta.y * Time.deltaTime,0);
-    }
-    hit = Physics2D.BoxCast(transform.position,boxCollider.size,0,new Vector2(moveDelta.x,0),Mathf.Abs(moveDelta.x * Time.deltaTime),LayerMask.GetMask("Actor","Blocker"));
-    if(hit.collider == null){
-        //Move Player on X
-        transform.Translate(moveDelta.x * Time.deltaTime,0,0);
-    }
-    }
+
 
 }
